dbc2f0eabc0d8acba4b0c9e9edbbdc8344470f38
bay <bhodza02@proton.me>
2026-09-20 11:15:27 -0500

    rename cask to luksmith

--- Casks/luksmith.rb
+++ Casks/luksmith.rb
@@ -1,15 +1,15 @@
-cask "luksmac" do
+cask "luksmith" do
   version "1.0.3"
   sha256 "612ff6212deec0d6d52a0948e7317e86c975bb5dc7eaa4bbf16d90a6239077a2"
 
-  url "https://github.com/Betim-Hodza/luksmac-releases/releases/download/v#{version}/LuksMac-#{version}.pkg"
-  name "LuksMac"
+  url "https://github.com/Betim-Hodza/luksmith-releases/releases/download/v#{version}/Luksmith-#{version}.pkg"
+  name "Luksmith"
   desc "Mount and manage LUKS-encrypted ext filesystems"
-  homepage "https://luksmac.app/"
+  homepage "https://luksmith.app/"
 
   depends_on macos: :tahoe
 
-  pkg "LuksMac-#{version}.pkg"
+  pkg "Luksmith-#{version}.pkg"
 
   uninstall pkgutil: "com.bay.luksmacos.pkg"
 end
